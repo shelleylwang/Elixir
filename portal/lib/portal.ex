4defb908ecf725d694ad6532550f38e3bd93f512
Shelley Wang <seashellsw@gmail.com>
2024-04-25 14:18:45 -0400

    removed portal/defmodule portal/door.ex portal/portal. Added portal/.DS_store and portal/lib/.DS_store. Changes to portal/lib/poral.ex

--- portal/lib/portal.ex
+++ portal/lib/portal.ex
@@ -1,29 +1,32 @@
-defstruct [:left, :right]
+defmodule Portal do
 
-@doc """
-Starts transfering `data` from `left` to `right`.
-"""
-def transfer(left, right, data) do
-  # First add all data to the portal on the left
-  for item <- data do
-    Portal.Door.push(left, item)
+  defstruct [:left, :right]
+
+  @doc """
+  Starts transfering `data` from `left` to `right`.
+  """
+  def transfer(left, right, data) do
+    # First add all data to the portal on the left
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    # Returns a portal struct we will use next
+    %Portal{left: left, right: right}
   end
 
-  # Returns a portal struct we will use next
-  %Portal{left: left, right: right}
-end
+  @doc """
+  Pushes data to the right in the given `portal`.
+  """
+  def push_right(portal) do
+    # See if we can pop data from left. If so, push the
+    # popped data to the right. Otherwise, do nothing.
+    case Portal.Door.pop(portal.left) do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
 
-@doc """
-Pushes data to the right in the given `portal`.
-"""
-def push_right(portal) do
-  # See if we can pop data from left. If so, push the
-  # popped data to the right. Otherwise, do nothing.
-  case Portal.Door.pop(portal.left) do
-    :error -> :ok
-    {:ok, h} -> Portal.Door.push(portal.right, h)
+    # Let's return the portal itself
+    portal
   end
-
-  # Let's return the portal itself
-  portal
 end
